9446e6dce4880c4a25d11356f2a623634a94224b
Marcus Jones <jones.0bj3@gmail.com>
2025-08-08 18:11:00 +0200

    update imports

--- src/climate_utils/__init__.py
+++ src/climate_utils/__init__.py
@@ -7,43 +7,8 @@ and environmental analysis.
 
 __version__ = "0.1.0"
 
-# Import main modules for easy access
-from . import epw
-from . import wind
-from . import solar
-from . import state_point
-from . import wind_analysis
+# Import all modules - users can access functions via module names
+# e.g., climate_utils.epw.load_epw_with_location()
+from . import epw, wind, solar, state_point, wind_analysis, types, psychrometrics_utils
 
-# Import commonly used functions
-from .epw import load_epw, load_epw_to_df, epw_to_df
-from .wind import adjust_wind_speed, map_wind_direction_to_sector
-from .solar import get_surface_irradiation_orientations_epw
-from .state_point import StatePoint, create_state_point_from_epw
-from .wind_analysis import (
-    adjust_wind_speed_height,
-    calculate_wind_rose_data,
-    calculate_wind_statistics,
-    analyze_wind_resource,
-)
-
-__all__ = [
-    # Modules
-    "epw",
-    "wind",
-    "solar",
-    "state_point",
-    "wind_analysis",
-    # Common functions
-    "load_epw",
-    "load_epw_to_df",
-    "epw_to_df",
-    "adjust_wind_speed",
-    "map_wind_direction_to_sector",
-    "get_surface_irradiation_orientations_epw",
-    "StatePoint",
-    "create_state_point_from_epw",
-    "adjust_wind_speed_height",
-    "calculate_wind_rose_data",
-    "calculate_wind_statistics",
-    "analyze_wind_resource",
-]
+__all__ = ["epw", "wind", "solar", "state_point", "wind_analysis", "types", "psychrometrics_utils"]
